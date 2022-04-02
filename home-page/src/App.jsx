2667f57a5f7cd879be11846978f616038935f338
cam090 <cam.courtman-stock@sky.com>
2022-04-02 18:38:46 +0100

    feat: basic chat app

---
+++ home-page/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Container } from 'shards-react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'shards-ui/dist/css/shards.min.css';
+
+import './index.css';
+
+import Chat from 'chat/Chat';
+
+const App = () => (
+  <Container>
+    <p>
+      asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasd
+    </p>
+    <h1>Chat!</h1>
+    <Chat />
+    <p>
+      asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasd
+    </p>
+  </Container>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
